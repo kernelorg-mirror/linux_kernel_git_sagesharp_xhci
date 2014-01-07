9d0fc035179b1cf41b3a1ab07fccaa40bd4dade6
Dan Williams <dan.j.williams@intel.com>
2014-01-07 12:30:00 -0800

    usb: gate clearing FEAT_C_ENABLE to usb2 hubs
    The port pm_runtime implementation unconditionally clears FEAT_C_ENABLE
    after clearing PORT_POWER, but the bit is reserved on usb3 hub ports.
    
    It also takes steps to re-disable the port if it fails to reconnect
    which again is broken on usb3 ports and unnecessary on usb2 ports.
    
    Signed-off-by: Dan Williams <dan.j.williams@intel.com>

--- drivers/usb/core/port.c
+++ drivers/usb/core/port.c
@@ -102,7 +102,6 @@ static int usb_port_runtime_resume(struct device *dev)
 		if (retval < 0)
 			dev_dbg(&port_dev->dev, "can't get reconnection after setting port  power on, status %d\n",
 					retval);
-		usb_clear_port_feature(hdev, port1, USB_PORT_FEAT_C_ENABLE);
 		retval = 0;
 	}
 
@@ -142,7 +141,8 @@ static int usb_port_runtime_suspend(struct device *dev)
 	set_bit(port1, hub->busy_bits);
 	retval = usb_hub_set_port_power(hdev, hub, port1, false);
 	usb_clear_port_feature(hdev, port1, USB_PORT_FEAT_C_CONNECTION);
-	usb_clear_port_feature(hdev, port1,	USB_PORT_FEAT_C_ENABLE);
+	if (!hub_is_superspeed(hdev))
+		usb_clear_port_feature(hdev, port1, USB_PORT_FEAT_C_ENABLE);
 	clear_bit(port1, hub->busy_bits);
 	usb_autopm_put_interface(intf);
 
